74d641ef917efef17256daefa80a30c966e775e8
Gyan888 <gyanender@agrichain.com>
2020-09-07 10:45:45 +0530

    story-318367016 | adding date picker

--- src/Dashboard/ActionTable.jsx
+++ src/Dashboard/ActionTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,6 +7,16 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import { IconButton, ListItem, ListItemText, Avatar, Grid } from '@material-ui/core';
+import DateRangeTwoToneIcon from '@material-ui/icons/DateRangeTwoTone';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import InsertDriveFileRoundedIcon from '@material-ui/icons/InsertDriveFileRounded';
+import {get} from 'lodash';
+import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+
+
 
 const useStyles = makeStyles({
   table: {
@@ -14,19 +24,180 @@ const useStyles = makeStyles({
   },
   tableHeader:{
     backgroundColor: '#d3d3d36b'
+  },
+  price:{
+    color: '#c9d069',
+    paddingRight: '3px',
+    marginLeft: '4px'
+  },
+  iconButton:{
+    fontSize: 'inherit',
+  },
+  dateItem:{
+    textAlign: 'center',
+    position: 'relative'
+  },
+  avatar:{
+    position: 'relative',
+    left: '72px',
+  },
+  calender:{
+    color:'lightblue'
+  },
+  reports:{
+    color: 'red'
+  },
+  file:{
+    color: '#95e895'
   }
 });
 
-let createData = (name, calories, fat, carbs, protein) =>{
-  return { name, calories, fat, carbs, protein };
+let Pricing = (props) =>{
+  const classes = useStyles();
+  return (<span>
+    <IconButton  aria-label="add an alarm" classes={{
+      root: classes.iconButton
+    }}>
+      <MonetizationOnIcon className={classes.price}/>
+      View Pricing
+    </IconButton>
+  </span>)
+
 }
 
+let DateItem = props =>{
+  const classes = useStyles();
+
+  return (<span>
+    <ListItem classes={{
+      root: classes.dateItem
+    }}>
+      <ListItemText primary={get(props, 'date')} secondary={get(props, 'days_left')} />
+    </ListItem>
+  </span>)
+
+}
+
+let Campaign = props =>{
+    const classes = useStyles();
+    return (
+      <ListItem classes={{
+        root: classes.dateItem
+      }}>
+          <Avatar variant="square" alt='Game logo' src={props.image_url} />
+        <ListItemText primary={props.campaign} secondary={props.region} />
+      </ListItem>
+    )
+};
+
+let Actions = props =>{
+    const classes = useStyles();
+
+    let [actionButtons, setActionButtons] = useState({
+      'schedullar': false,
+      'csv': false,
+      'report': false
+    });
+
+    return (<span>
+      <Grid container direction='column'>
+        <Grid item container>
+        <Grid item sm={2} xs={0}>
+        </Grid>
+        <Grid item sm={3} xs={4}>
+        <IconButton  aria-label="get csv" classes={{root: classes.iconButton}}>
+          <InsertDriveFileRoundedIcon className={classes.file}/> CSV
+        </IconButton>
+        </Grid>
+
+        <Grid item sm={3} xs={4}>
+        <IconButton  aria-label="get report" classes={{root: classes.iconButton}}>
+          <TrendingUpIcon className={classes.reports}/> Report
+        </IconButton>
+        </Grid>
+
+        <Grid item sm={4} xs={4}>
+        <IconButton  aria-label="reschedule" classes={{root: classes.iconButton}} onClick={()=>{setActionButtons({
+          ...actionButtons, 'schedullar': !actionButtons.schedullar
+        })}}>
+
+          <DateRangeTwoToneIcon className={classes.calender}/> Schedule Again
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+
+              <DatePicker variant="inline" open={actionButtons.schedullar}
+              TextFieldComponent={() => null}/>
+          </MuiPickersUtilsProvider>
+        </IconButton>
+        </Grid>
+
+        </Grid>
+      </Grid>
+    </span>);
+};
+
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  {
+    "campaign": "PubG",
+    "region": "US",
+    "date": 'Oct 2019, 28',
+    'days_left': '23 days ago',
+    "price": 200,
+    "csv": "Some CSV link for Whatsapp",
+    "report": "Some report link for Whatsapp",
+    "image_url":"static/games/Bitmap.png"
+
+  },
+  {
+    "campaign": "PubG",
+    "region": "US",
+    "date": 'Oct 2019, 28',
+    'days_left': '23 days ago',
+    "price": 200,
+    "csv": "Some CSV link for Whatsapp",
+    "report": "Some report link for Whatsapp",
+    "image_url":"static/games/Bitmap.png"
+
+  },{
+    "campaign": "PubG",
+    "region": "US",
+    "date": 'Oct 2019, 28',
+    'days_left': '23 days ago',
+    "price": 200,
+    "csv": "Some CSV link for Whatsapp",
+    "report": "Some report link for Whatsapp",
+    "image_url":"static/games/Bitmap.png"
+
+  },{
+    "campaign": "PubG",
+    "region": "US",
+    "date": 'Oct 2019, 28',
+    'days_left': '23 days ago',
+    "price": 200,
+    "csv": "Some CSV link for Whatsapp",
+    "report": "Some report link for Whatsapp",
+    "image_url":"static/games/Bitmap.png"
+
+  },{
+    "campaign": "PubG",
+    "region": "US",
+    "date": 'Oct 2019, 28',
+    'days_left': '23 days ago',
+    "price": 200,
+    "csv": "Some CSV link for Whatsapp",
+    "report": "Some report link for Whatsapp",
+    "image_url":"static/games/Bitmap.png"
+
+  },{
+    "campaign": "PubG",
+    "region": "US",
+    "date": 'Oct 2019, 28',
+    'days_left': '23 days ago',
+    "price": 200,
+    "csv": "Some CSV link for Whatsapp",
+    "report": "Some report link for Whatsapp",
+    "image_url":"static/games/Bitmap.png"
+
+  },
 ];
 
 let  ActionTable = () =>{
@@ -37,21 +208,30 @@ let  ActionTable = () =>{
       <Table className={classes.table} aria-label="simple table">
         <TableHead className={classes.tableHeader}>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell align="right">Campaign</TableCell>
-            <TableCell align="right">View(g)</TableCell>
-            <TableCell align="right">Actions(g)</TableCell>
+            <TableCell align="center">Date</TableCell>
+            <TableCell align="center">Campaign</TableCell>
+            <TableCell align="center">View</TableCell>
+            <TableCell align="center">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
+              <TableCell  align="left">
+                <DateItem {...row}/>
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
+
+              <TableCell  align="center">
+                <Campaign {...row}/>
+              </TableCell>
+
+              <TableCell  align="center" >
+                <Pricing/>
+              </TableCell>
+
+              <TableCell  align="center">
+                <Actions {...row}/>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
