ec77323c00ba265d0e6604f6dcf892de50059ffd
Gyan888 <gyanender@agrichain.com>
2020-09-08 22:10:35 +0530

    Updates

---
+++ precache-manifest.c51c17af26e73d07c1268b990b1c75f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3551a99b704cd216d2549e3de9a4e1e",
+    "url": "/blueStacks/index.html"
+  },
+  {
+    "revision": "a2e6c52714a89fab13da",
+    "url": "/blueStacks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "844ccf0e1fcaaac08677",
+    "url": "/blueStacks/static/js/2.ebb0f535.chunk.js"
+  },
+  {
+    "revision": "f1228651ed12f837338c31fb55903ecf",
+    "url": "/blueStacks/static/js/2.ebb0f535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e6c52714a89fab13da",
+    "url": "/blueStacks/static/js/main.c97f8d45.chunk.js"
+  },
+  {
+    "revision": "609fa56910c0a023f4d9",
+    "url": "/blueStacks/static/js/runtime-main.f1bbf1a1.js"
+  }
+]);
